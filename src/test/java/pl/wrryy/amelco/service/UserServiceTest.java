b07212814e2d21731dbd2a1fa6f4a6427dcafcda
Wojciech Rydzewski <woj.rydzewski@gmail.com>
2018-02-15 16:15:51 +0100

    bety git

---
+++ src/test/java/pl/wrryy/amelco/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package pl.wrryy.amelco.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @Test
+    public void registerUser() {
+    }
+
+    @Test
+    public void walletPay() {
+    }
+
+    @Test
+    public void hasEnoughWalletBalance() {
+    }
+}
